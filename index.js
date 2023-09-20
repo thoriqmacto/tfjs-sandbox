3a5c95f6f5cd9e4153f631166823f8cfd0ece191
thoriqmacto <thariq.hadad@gmail.com>
2023-09-20 21:09:44 +0700

    add ordered list to the predictions

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
+var predictions = [];
+
 async function app() {
   console.log('App Started...');
 
+  var predictionContainer = document.getElementById('predictionsContainer');
   const webcamElement = document.getElementsByTagName('video')[0];
   const model = await mobilenet.load();
 
   const webcam = await tf.data.webcam(webcamElement);
   const captureButton = document.getElementsByTagName('button')[0];
+
   captureButton.onclick = async () => {
     const img = await webcam.capture();
-    const predictions = await model.classify(img);
+    predictions = await model.classify(img);
     img.dispose();
-    console.log(predictions);
-    return predictions;
+
+    if (predictions.length > 0) {
+      /* console.log(predictions);
+      console.log('Total predictions:', predictions.length);
+
+      predictions.forEach((element) => {
+        console.log('Classname:', element.className);
+        console.log('Probability:', element.probability);
+      }); */
+
+      // Clear inner HTML
+      predictionContainer.innerHTML = '';
+
+      // Create ordered list
+      var orderedList = document.createElement('ol');
+
+      // Loop through the array of predictions
+      predictions.forEach((e) => {
+        const probabilityInPercentage = (e.probability * 100).toFixed(2) + '%';
+        var listItem = document.createElement('li');
+
+        var predictTextBold = document.createElement('strong');
+        predictTextBold.textContent = 'Predictions:';
+
+        var probabilityTextBold = document.createElement('strong');
+        probabilityTextBold.textContent = 'Probability:';
+
+        listItem.appendChild(predictTextBold);
+        listItem.appendChild(
+          document.createTextNode(' ' + e.className + ' | ')
+        );
+        listItem.appendChild(probabilityTextBold);
+        listItem.appendChild(
+          document.createTextNode(' ' + probabilityInPercentage)
+        );
+        orderedList.appendChild(listItem);
+      });
+      predictionContainer.appendChild(orderedList);
+    }
   };
 }
 app();
